0541280db755992a95ad6164b84fc4a7d7c8b985
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 17:16:30 +0200

    Processing creates a `Message` and transmits it
    Quite a crude implementation for now, and the mailer is yet to be
    writter, too.

--- app/mailboxes/messages_mailbox.rb
+++ app/mailboxes/messages_mailbox.rb
@@ -7,6 +7,12 @@ class MessagesMailbox < ApplicationMailbox
   before_processing :bounced!, if: -> { game.blank? || wrong_sender? }
   
   def process
+    sender    = game.players.find { |p| mail.from.include? p.email }
+    content   = mail.body.to_s
+    
+    if message = game.messages.create(sender:, content:, subject: mail.subject)
+      MessagesMailer.with(message:).transmission.deliver_later
+    end
   end
   
   def set_game
