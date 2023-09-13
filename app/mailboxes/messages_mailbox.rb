1278a81b381aa5ef5304f08cfdd9b09e182416a5
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 13:25:15 +0200

    Boucing mails sent by a non-player in the game
    Two things to notice:
    
    - You cannot independently declare callbacks on the same event,
      with the same result, even if they have different conditions (like
      `before_process :foo, if: :bar?` and `before_process :foo, if: :baz?`). The
      two conditions must be merged in a single declaration
      (`before_process :foo, if: -> { bar? || baz? }}`)
    - More of a stylistic hair-splitting, I've use `or` instead of `||` when
      combining the conditions, because there is a bit of flow control here:
      because the first condition _also_ loads the `@game` instance variable,
      and said variable is needed by the second condition. Yes, it's a bit
      smelly.

--- app/mailboxes/messages_mailbox.rb
+++ app/mailboxes/messages_mailbox.rb
@@ -1,12 +1,16 @@
 class MessagesMailbox < ApplicationMailbox
   MATCHER = /^([\-\w]+)@mavrides.example/i
   
-  before_processing :bounced!, if: -> { game.blank? }
+  before_processing :bounced!, if: -> { game.blank? or wrong_sender? }
   
   def process
   end
   
   def game
     @game ||= Game.find_by(id: mail.to.grep(MATCHER) { $1 })
+  end
+  
+  def wrong_sender?
+    game.players.none? { |p| mail.from.include? p.email }
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ApplicationRecord
   belongs_to :earther, class_name: "Player"
   belongs_to :explorer, class_name: "Player"
+  
+  def players
+    [earther, explorer]
+  end
 end
