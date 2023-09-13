5954d761b39dffc739cd0b48cd60cafeed97498f
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 17:26:30 +0200

    Mailbox: DRY up the logic for fetching a sender
    We need it twice: once to validate that the email is valid, and once
    to create the `Message`. So it's worth being stored in an instance
    variable. And this refactoring helps us clean up the code a bit (e.g.
    by removing a useless memoization left there by accident.)

--- app/mailboxes/messages_mailbox.rb
+++ app/mailboxes/messages_mailbox.rb
@@ -1,25 +1,29 @@
 class MessagesMailbox < ApplicationMailbox
   MATCHER = /^([\-\w]+)@mavrides.example/i
 
-  attr_reader :game
+  attr_reader :game, :sender
 
-  before_processing :set_game
-  before_processing :bounced!, if: -> { game.blank? || wrong_sender? }
+  before_processing :set_game_and_sender
+  before_processing :bounced!, if: -> { game.blank? || sender.blank? }
 
   def process
-    sender    = Player.where(email: mail.from).participating_in(game)
-    content   = mail.body.to_s
+    content = mail.body.to_s
 
     if message = game.messages.create(sender:, content:, subject: mail.subject)
       MessagesMailer.with(message:).transmission.deliver_later
     end
   end
 
-  def set_game
-    @game ||= Game.running.find_by(id: mail.to.grep(MATCHER) { $1 })
-  end
+  private
 
-  def wrong_sender?
-    Player.where(email: mail.from).participating_in(game).none?
+  def set_game_and_sender
+    @game   = Game.running.find_by(id: mail.to.grep(MATCHER) { $1 })
+    unless @game.blank?
+      @sender = begin
+                  Player.where(email: mail.from).participating_in(@game).sole
+                rescue ActiveRecord::RecordNotFound
+                  nil
+                end
+    end
   end
 end
