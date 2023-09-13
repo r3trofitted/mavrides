dee9ed9662f19756bc41b15773dc4d8ec19d9bd5
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 20:04:36 +0200

    Basic implem. of MessagesMailer#transmission
    The views aren't proper yet, but the logic (as little as it is) is there,
    TDD'ed. (Interstingly, I choose not to use fixtures for the mail's body,
    contrary to what's advised in the Rails Guides.)

---
+++ test/mailers/messages_mailer_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class MessagesMailerTest < ActionMailer::TestCase
+  fixtures :all
+
+  test "transmission" do
+    mail = MessagesMailer.with(message: messages(:first_message)).transmission
+
+    assert_equal "さようなら", mail.subject
+    assert_equal ["bruce.s@mavrides.example"], mail.from
+    assert_equal ["vincent.o@mavrides.example"], mail.to
+
+    assert_equal <<~TXT, mail.text_part.body.to_s.chomp # I'm not sure why chomp is needed, TBH, but here we are…
+      This is it. We are off. To Tau Ceti, can you believe it? Even though I'll never see what's out there, I know.
+    TXT
+  end
+end
