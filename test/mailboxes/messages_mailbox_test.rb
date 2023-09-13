c887529fc238b2c8c0843dd88b0b64110f6d988b
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 10:32:02 +0200

    Test case: bouncing mails from a non-player
    If the `to` address is valid, but the sender is not a player in the
    associated game, the email should bouce, too.

--- test/mailboxes/messages_mailbox_test.rb
+++ test/mailboxes/messages_mailbox_test.rb
@@ -20,4 +20,14 @@ class MessagesMailboxTest < ActionMailbox::TestCase
   
   refute inbound_email.bounced?
   end
+  
+  test "bounce mail for a game in which the sender doesn't participate" do
+    inbound_email = receive_inbound_email_from_mail(
+      to: "#{games(:abelar_and_philip).id}@mavrides.example",
+      from: '"Unknown player" <korolev@example.com>',
+      subject: "White orbit"
+    )
+  
+    assert inbound_email.bounced?
+  end
 end
