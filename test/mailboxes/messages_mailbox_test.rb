faf594b1fe43981df8ad905ac624c9bfb4ae8608
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 15:49:32 +0200

    Test: transmitting a valid message
    This introduces 3 new things:
    
    *   The incoming mail fixtures (which were easy to generate thanks with
        the conductor UI at
        `http://localhost:3000/rails/conductor/action_mailbox/inbound_emails`
    *   The necessity to manually include `ActionMailer::TestHelper` in the
        test case in order to test the _sending_ of emails when processing
        _incoming_ emails.
    *   The idea that the _messages_ sent in a given game should be kept.
    
    This means that the processing of a message mail should have 2 effects:
    a new `Message` record should be created, and a new mail should be
    sent to transmit the message to its actual recipient. This makes our
    test more of an integration test. Which is fine by me.

--- test/mailboxes/messages_mailbox_test.rb
+++ test/mailboxes/messages_mailbox_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class MessagesMailboxTest < ActionMailbox::TestCase
+  include ActionMailer::TestHelper
+  
   setup do
+    ActionMailer::Base.deliveries.clear
+    
     @game = games(:abelar_and_philip)
   end
   
@@ -25,13 +29,18 @@ class MessagesMailboxTest < ActionMailbox::TestCase
     assert inbound_email.bounced?
   end
     
-  test "process mail for known games" do
-    inbound_email = receive_inbound_email_from_mail(
-      to: "#{@game.id}@mavrides.example",
-      from: '"Abelar" <abelar@mavrides.example>',
-      subject: "Goodbye, Earth"
-    )
+  test "transmit message to the other player" do
+    assert_difference -> { @game.messages.count } do
+      receive_inbound_email_from_fixture "first_message.eml"
+    end
     
-    refute inbound_email.bounced?
+    message = Message.last
+    assert_equal players(:abelar), message.sender
+    assert_equal "さようなら", message.subject
+    assert_equal <<~TXT, message.content
+      This is it. We are off. To Tau Ceti, can you believe it? Even though I'll never see what's out there, I know.
+    TXT
+    
+    assert_enqueued_email_with MessagesMailer, :transmission, params: { message: message }
   end
 end
