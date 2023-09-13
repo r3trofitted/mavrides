0722e9b1fa0d0846f4f06cc30dd89b99a9547075
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-13 10:28:17 +0200

    DRYing up the matching rule for the messages

--- app/mailboxes/messages_mailbox.rb
+++ app/mailboxes/messages_mailbox.rb
@@ -1,10 +1,12 @@
 class MessagesMailbox < ApplicationMailbox
+  MATCHER = /^([\-\w]+)@mavrides.example/i
+  
   before_processing :bounced!, if: -> { game.blank? }
   
   def process
   end
 
   def game
-    @game ||= Game.running.find_by(id: mail.to.grep(/^([\-\w]+)@mavrides.example/) { $1 }) # FIXME: don't hardcode the domain
+    @game ||= Game.running.find_by(id: mail.to.grep(MATCHER) { $1 })
   end
 end
