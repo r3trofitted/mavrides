9cf10e20c1a147ef225ca9ca6ece4ef68ed4ab37
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 20:53:25 +0200

    Added Card#<=> and made it Enumerable
    Because the cards of a hand need to be sorted if we want their
    serialization to be deterministic (and hence testable).
    
    Because this adds even more logic to the Card class, I'm letting go
    of the `Data` stuff – let's have a real, simple, albeit mutation-enabling
    class.

--- lib/card.rb
+++ lib/card.rb
@@ -1,15 +1,19 @@
-Card = Data.define(:value, :suit) do
-  self::SUITS  = %i[clubs diamonds hearts spades]
-  self::VALUES = %i[1 2 3 4 5 6 7 8 9 jack queen king ace]
-  self::EMOJIS = { spades: "♠️", clubs: "♣️", hearts: "♥️", diamonds: "♦️" }
+class Card
+  include Comparable
+
+  SUITS  = %i[clubs diamonds hearts spades]
+  VALUES = %i[1 2 3 4 5 6 7 8 9 jack queen king ace]
+  EMOJIS = { spades: "♠️", clubs: "♣️", hearts: "♥️", diamonds: "♦️" }
+
+  attr_reader :value, :suit
 
   class << self
     def load(payload)
       if payload.present?
         v, s = payload.scan(/\X/) # \X matches a single Unicode grapheme (necessary because the emojis are grapheme _clusters_)
 
-        value = Card::VALUES.detect { |s| s.start_with?(/#{v}/i) }
-        suit  = Card::EMOJIS.key(s)
+        value = VALUES.detect { |s| s.start_with?(/#{v}/i) }
+        suit  = EMOJIS.key(s)
 
         Card.new value:, suit:
       end
@@ -20,18 +24,26 @@ Card = Data.define(:value, :suit) do
     end
 
     def new_hand
-      Card::SUITS.map { |s| new(value: Card::VALUES.sample, suit: s) }
+      SUITS.map { |s| new(value: VALUES.sample, suit: s) }
     end
   end
 
   def initialize(value:, suit:)
-    value = value.to_s.to_sym.presence_in(Card::VALUES) or raise ArgumentError
-    suit  = suit.to_sym.presence_in(Card::SUITS) or raise ArgumentError
-
-    super value:, suit:
+    @value = value.to_s.to_sym.presence_in(VALUES) or raise ArgumentError
+    @suit  = suit.to_sym.presence_in(SUITS) or raise ArgumentError
   end
 
   def to_short_s
-    value[0].upcase << Card::EMOJIS.fetch(suit)
+    @value[0].upcase << EMOJIS.fetch(suit)
+  end
+
+  def <=>(other)
+    return nil unless other.kind_of? Card
+
+    if suit == other.suit
+      value <=> other.value
+    else
+      suit <=> other.suit
+    end
   end
 end
