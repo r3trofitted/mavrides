194a7c7884412fbd61af10e57198216f742652a4
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 08:54:32 +0200

    Card: short representation
    A lot of cool stuff here:
    
    *   Let's use emojis! It's childish and will possibly cause issues when
        it's time to store things in the DB, but it's fun!
    *   Because I want to have constants for the suits and values, we need
        to use a class and not a `Struct` or a `Data`, since the latter are
        PITAs when it comes to constants.
    *   Writing the test for the string representation lead us to some
        discoveries about the DSL we want for the _initialization_ of new
        `Card` objects. So let's test and implement that better.

---
+++ lib/card.rb
@@ -0,0 +1,26 @@
+class Card
+  SUITS = [
+    CLUBS    = "♣️",
+    DIAMONDS = "♦️",
+    HEARTS   = "♥️",
+    SPADES   = "♠️"
+  ]
+
+  VALUES = [
+    ONE  = "1", TWO = "2",  THREE = "3", FOUR  = "4",
+    FIVE = "5", SIX = "6",  SEVEN = "7", EIGHT = "8",
+    NINE = "9", TEN = "10", JACK  = "J", QUEEN = "Q",
+    KING = "K", ACE = "A"
+  ]
+
+  def initialize(value:, suit:)
+    value = Card.const_get(value.upcase) if value.is_a? Symbol
+    suit  = Card.const_get(suit.upcase) if suit.is_a? Symbol
+
+    @value, @suit = value.to_s, suit
+  end
+
+  def to_short_s
+    @value << @suit
+  end
+end
