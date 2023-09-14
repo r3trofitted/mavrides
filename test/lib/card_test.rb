11bfd6ebce925be1a82452f3fd0e539a8d52e38a
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 20:53:25 +0200

    Added Card#<=> and made it Enumerable
    Because the cards of a hand need to be sorted if we want their
    serialization to be deterministic (and hence testable).
    
    Because this adds even more logic to the Card class, I'm letting go
    of the `Data` stuff – let's have a real, simple, albeit mutation-enabling
    class.

--- test/lib/card_test.rb
+++ test/lib/card_test.rb
@@ -22,4 +22,26 @@ class CardTest < ActiveSupport::TestCase
     assert_equal 4, hand.count
     assert_equal 4, hand.map(&:suit).uniq.count # 1 card of each suit
   end
+  
+  test "comparing cards with different suits" do
+    spades   = Card.new(value: 7, suit: :spades)
+    clubs    = Card.new(value: 7, suit: :clubs)
+    hearts   = Card.new(value: 7, suit: :hearts)
+    diamonds = Card.new(value: 7, suit: :diamonds)
+    
+    assert clubs < diamonds
+    assert diamonds < hearts
+    assert hearts < spades
+  end
+  
+  test "comparing cards with the same suits" do
+    queen = Card.new(value: :queen, suit: :spades)
+    ace   = Card.new(value: :ace, suit: :spades)
+    eight = Card.new(value: 8, suit: :spades)
+    seven = Card.new(value: 7, suit: :spades)
+    
+    assert seven < eight
+    assert eight < ace
+    assert ace < queen
+  end
 end
