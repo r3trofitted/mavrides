282fa3b25a93837ad69fadcc14bccc9bd2525f4f
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 13:11:35 +0200

    f card

--- lib/card.rb
+++ lib/card.rb
@@ -3,11 +3,13 @@ Card = Data.define(:value, :suit) do
   self::VALUES = %i[1 2 3 4 5 6 7 8 9 jack queen king ace]
   
   def initialize(value:, suit:)
-    @value = value.to_s.to_sym.presence_in(Card::VALUES) or raise ArgumentError
-    @suit  = suit.to_sym.presence_in(Card::SUITS) or raise ArgumentError
+    value = value.to_s.to_sym.presence_in(Card::VALUES) or raise ArgumentError
+    suit  = suit.to_sym.presence_in(Card::SUITS) or raise ArgumentError
+    
+    super(value:, suit:)
   end
   
   def to_short_s
-    @value[0].upcase << { spades: "♠️", clubs: "♣️", hearts: "♥️", diamonds: "♦️" }.fetch(@suit)
+    value[0].upcase << { spades: "♠️", clubs: "♣️", hearts: "♥️", diamonds: "♦️" }.fetch(suit)
   end
 end
