753797171b1fcbe747ba84a4b6640317bffdf125
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 12:54:33 +0200

    f test card

--- test/lib/card_test.rb
+++ test/lib/card_test.rb
@@ -1,13 +1,6 @@
 require "test_helper"
 
 class CardTest < ActiveSupport::TestCase
-  test "new card" do
-    assert Card.new value: 3, suit: :spades
-    
-    assert_raises(ArgumentError) { assert Card.new value: 13, suit: :spades } # invalid value
-    assert_raises(ArgumentError) { assert Card.new value: 3, suit: :cups }    # invalid suit
-  end
-  
   test "short string representation" do
     assert_equal "3♠️", Card.new(value: 3, suit: :spades).to_short_s
     assert_equal "A♥️", Card.new(value: :ace, suit: :hearts).to_short_s
