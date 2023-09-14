03054c5e4da8ed3d8c9a66e36c82fe721e9d91a8
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 10:20:25 +0200

    Removed the "flexible initializer"
    Being able to create `Card` objects by passing symbols, ints, strings,
    or constants names makes for a friendly DSL, but it brings a lot of
    internal complexity. It's not worth it. By restricting the API, we
    can also remove a lot of code, to the point that using `Data` objects
    makes sense again. Even if the code for `#to_short_s` is a bit
    cryptics, I find the final result friendlier.

--- test/lib/card_test.rb
+++ test/lib/card_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 class CardTest < ActiveSupport::TestCase
   test "short string representation" do
-    assert_equal "3♠️", Card.new(value: 3, suit: Card::SPADES).to_short_s
-    assert_equal "K♦️", Card.new(value: Card::KING, suit: Card::DIAMONDS).to_short_s
-    assert_equal "A♥️", Card.new(value: :ace, suit: :hearts).to_short_s            # alternative notation
-    assert_equal "8♣️", Card.new(value: "8", suit: :clubs).to_short_s
+    assert_equal "3♠️", Card.new(value: 3, suit: :spades).to_short_s
+    assert_equal "A♥️", Card.new(value: :ace, suit: :hearts).to_short_s
+    assert_equal "K♦️", Card.new(value: :king, suit: :diamonds).to_short_s
+    assert_equal "8♣️", Card.new(value: 8, suit: :clubs).to_short_s
   end
 end
