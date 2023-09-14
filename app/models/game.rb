6564bff1200e6dd704fd6d63999c9eecc185585b
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 12:55:37 +0200

    Adding a message to a game can create a new roud
    (If the message is sent by the explorer, that is, since according to
    the rules, the explorer starts the first – and thus every subsequent one.)

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,10 +2,22 @@ class Game < ApplicationRecord
   belongs_to :earther, class_name: "Player"
   belongs_to :explorer, class_name: "Player"
   
-  has_many :rounds, -> { order(number: :asc) }
-  has_many :messages
+  has_many :rounds, -> { order(number: :asc) } do
+    def current
+      last
+    end
+    
+    def next!
+      create!(number: current&.number.to_i + 1)
+    end
+  end
+  has_many :messages, after_add: :update_rounds
   
   def players
     [earther, explorer]
+  end
+  
+  def update_rounds(message)
+    rounds.next! if message.sender == explorer
   end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a new round starts when the explorer sends their message" do
+    game = Game.create explorer: players(:abelar), earther: players(:philip)
+    assert_empty game.rounds
+    
+    assert_difference ->{ game.rounds.count } do
+      game.messages << Message.new(sender: game.explorer)
+    end
+  end
+  
+  test "the current round ends when the earther sends their message" do
+    game = Game.create explorer: players(:abelar), earther: players(:philip)
+    game.messages.create! sender: game.explorer
+    refute_empty game.rounds
+    
+    assert_no_difference ->{ game.rounds.count } do
+      game.messages << Message.new(sender: game.earther)
+    end
+  end
 end
