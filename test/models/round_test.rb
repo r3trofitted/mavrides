7d851854938366746a25c402b3577639c2609797
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-14 14:15:42 +0200

    Added Round#next
    So that a new Round can be created _from_. previous one, handling the
    filling up of the hand (i.e. drawing new cards after an Event has been
    played). Note that the actual filling up is slimed, for now.

--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -7,4 +7,15 @@ class RoundTest < ActiveSupport::TestCase
     refute_empty round.earther_hand
     refute_empty round.explorer_hand
   end
+  
+  test "#next builds a new Round" do
+    new_round = rounds(:abelar_and_philip_round_one).next
+    
+    assert_equal rounds(:abelar_and_philip_round_one).game, new_round.game
+    assert_equal 2, new_round.number
+    assert_nil new_round.earther_event
+    assert_nil new_round.explorer_event
+    refute_nil new_round.earther_hand
+    refute_nil new_round.explorer_hand
+  end
 end
