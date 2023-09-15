0eeed3767e17313a2068e0654ab8a540e6219977
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-15 12:43:39 +0200

    Removed #fill_hand but kept the logic
    Now, this is an interesting commit. In a previous one, I introduced
    `Round#fill_hand`, as an implementation of the rule "Draw two cards from
    the draw pile that matches the suit of the current Event card. Place
    them into your hand.", which is part of the round sequence. (In Mavrides,
    the round sequence is pre-executed for both players when a new round
    starts, in `Round#next`.)
    
    This small piece of implementation had been put in its own method
    mostly to make is easier to test, since it would require a specific
    setup: previous events had to be prepared so for the remaining draw
    pile to be in a controlled state suitable for a test.
    
    However, working on it, I realized the logic behind the computation of
    the draw pile(s) should be under the `Game` responsibility, therefore
    simplifying the test setup, which in turn made the `#fill_hand` method
    less relevant. So I moved the significant parts of the test (including
    _a bit_ more setup) to the test for `Round#next`.

--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -8,32 +8,24 @@ class RoundTest < ActiveSupport::TestCase
     assert_not_empty round.explorer_hand
   end
 
-  test "#next builds a new Round" do
-    new_round = rounds(:running_game_round_one).next
-
-    assert_equal rounds(:running_game_round_one).game, new_round.game
-    assert_equal 2, new_round.number
-    assert_nil new_round.earther_event
-    assert_nil new_round.explorer_event
-    assert_not_nil new_round.earther_hand
-    assert_not_nil new_round.explorer_hand
-  end
-
-  test "#fill_hand adds 2 cards of the current event's type to the designated hand" do
-    round = Round.new(game: games(:running_game)) do |r|
+  test "#next builds a new Round with 2 cards added to each hand" do
+    round = Round.new do |r|
+      r.game           = games(:running_game)
+      r.number         = 2
       r.earther_event  = Card.new(value: 1, suit: :clubs)
       r.explorer_event = Card.new(value: :ace, suit: :spades)
-
-      # ensuring that the hands are empty to make the assertions simpler
-      r.earther_hand  = []
-      r.explorer_hand = []
+      r.earther_hand   = [] # ensuring that the hands are empty to make the assertions simpler
+      r.explorer_hand  = [] # ensuring that the hands are empty to make the assertions simpler
     end
 
-    round.fill_hand(:earther)
-    assert_equal 2, round.earther_hand.count { |c| c.suit == :clubs }
+    new_round = round.next
 
-    round.fill_hand(:explorer)
-    assert_equal 2, round.explorer_hand.count { |c| c.suit == :spades }
+    assert_equal rounds(:running_game_round_one).game, new_round.game
+    assert_equal 3, new_round.number
+    assert_nil new_round.earther_event
+    assert_nil new_round.explorer_event
+    assert_equal 2, new_round.earther_hand.count { |c| c.suit == :clubs }   # same suit as the event
+    assert_equal 2, new_round.explorer_hand.count { |c| c.suit == :spades } # same suit as the event
   end
 
   test "serializing events" do
