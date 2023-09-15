695145afa611a86190f1e1b0ccb24090af7c6b7d
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-15 14:24:27 +0200

    Tests for the logic of starting a new game
    Basically, we set up the first round and let the players know that
    the game has started.

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,6 +1,32 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
+
+  test "#starts creates the first round" do
+    game = Game.new earther: characters(:philip), explorer: characters(:abelar)
+
+    assert_difference ->{ game.rounds.count } do
+      game.starts
+    end
+  end
+
+  test "#starts sends a message to the Explorer" do
+    game = Game.new earther: characters(:philip), explorer: characters(:abelar)
+
+    assert_enqueued_email_with GameMailer, :game_starts_for_explorer, params: { game: game } do
+      game.starts
+    end
+  end
+
+  test "#starts sends a message to the Earther" do
+    game = Game.new earther: characters(:philip), explorer: characters(:abelar)
+
+    assert_enqueued_email_with GameMailer, :game_starts_for_earther, params: { game: game } do
+      game.starts
+    end
+  end
+
   test "a new round starts when the explorer player sends their message" do
     game = Game.create explorer: characters(:abelar), earther: characters(:philip)
     assert_empty game.rounds
