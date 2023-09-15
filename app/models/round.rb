e1692a80787fe203ab027fc70aea64b551ae4953
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-15 15:04:36 +0200

    New implem for Round#next
    To be clear: the rounds that are records are rounds in their
    _final_ state: with the 2 events drawn and the 2 hands refilled. So a
    `Round` created by `Round#next` has all its logic already applied: the
    events have been drawn from the hands of the current round, and the
    hands prepared with 2 new cards drawn.

--- app/models/round.rb
+++ app/models/round.rb
@@ -27,11 +27,19 @@ class Round < ApplicationRecord
   end
   
   def next
+    new_earther_event  = earther_hand.sample
+    new_explorer_event = explorer_hand.sample
+    
+    new_earther_hand  = (earther_hand - [new_earther_event]) | game.draw_pile_of(:earther, suit: new_earther_event.suit).sample(2)
+    new_explorer_hand = (explorer_hand - [new_explorer_event]) | game.draw_pile_of(:explorer, suit: new_explorer_event.suit).sample(2)
+    
     Round.new(
       game: game,
       number: number + 1,
-      earther_hand: earther_hand | game.draw_pile_of(:earther, suit: earther_event.suit).sample(2),
-      explorer_hand: explorer_hand | game.draw_pile_of(:explorer, suit: explorer_event.suit).sample(2)
+      earther_event: new_earther_event,
+      explorer_event: new_explorer_event,
+      earther_hand: new_earther_hand,
+      explorer_hand: new_explorer_hand
     )
   end
 end
