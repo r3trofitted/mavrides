c7620fe6c74038b4df13e834d2bdb4c910bac572
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-15 14:35:22 +0200

    Game#starts creates the first round
    Note that this alters the behavior of the app, and breaks some tests.
    More significantly, this reveals a missing piece in the Round logic:
    events are not drawn (yet).

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -29,7 +29,7 @@ class GameTest < ActiveSupport::TestCase
 
   test "a new round starts when the explorer player sends their message" do
     game = Game.create explorer: characters(:abelar), earther: characters(:philip)
-    assert_empty game.rounds
+    game.starts
 
     assert_difference ->{ game.rounds.count } do
       game.messages << Message.new(sender: game.explorer_player)
@@ -38,8 +38,7 @@ class GameTest < ActiveSupport::TestCase
 
   test "the current round ends when the earther player sends their message" do
     game = Game.create explorer: characters(:abelar), earther: characters(:philip)
-    game.messages << Message.new(sender: players(:bruce))
-    assert_not_empty game.rounds
+    game.starts
 
     assert_no_difference ->{ game.rounds.count } do
       game.messages << Message.new(sender: players(:vincent))
