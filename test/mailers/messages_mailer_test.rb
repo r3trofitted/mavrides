d6b2a7bb7e68c4f2dcfbed27ecf9ac47ecbae12c
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-15 19:45:15 +0200

    Emails will be text-only for now
    It's a tradeof to see the end of the project sooner. Because HTML emails
    is HARD.

--- test/mailers/messages_mailer_test.rb
+++ test/mailers/messages_mailer_test.rb
@@ -8,7 +8,7 @@ class MessagesMailerTest < ActionMailer::TestCase
     assert_equal ["abelar@mavrides.example"], mail.from
     assert_equal ["philip@mavrides.example"], mail.to
     
-    assert_equal <<~TXT, mail.text_part.body.to_s.chomp # I'm not sure why chomp is needed, TBH, but here we are…
+    assert_equal <<~TXT, mail.body.to_s.chomp # I'm not sure why chomp is needed, TBH, but here we are…
       This is it. We are off. To Tau Ceti, can you believe it? Even though I'll never see what's out there, I know.
     TXT
   end
