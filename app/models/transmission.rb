904a91f7354c916495120e6516102fc026967ee4
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-18 15:42:51 +0200

    Implementation for the basic distortion
    (We should add tests for the BasicDistortion class. And documentation.)

--- app/models/transmission.rb
+++ app/models/transmission.rb
@@ -3,6 +3,7 @@ class Transmission
   
   def initialize(message)
     @message = message
+    @distortion = BasicDistortion.new
   end
   
   def second_to_last?
@@ -17,6 +18,10 @@ class Transmission
     round.event_for(recipient).value # e.g. :ace
   end
   
+  def content
+    @distortion.apply @message.content, game.events_for(sender).map(&:value)
+  end
+  
   private
   
   def major_event_table
@@ -25,5 +30,37 @@ class Transmission
   
   def major_event_progress
     game.events_for(recipient, suit: major_event_table).count
+  end
+  
+  class BasicDistortion
+    SUBSTITUTIONS = {
+      "2":   [nil, ["Ee", "Rr"]],
+      "3":   [nil, ["Cc", "Oo"]],
+      "4":   [["Ll", "Ii"], nil],
+      "5":   [["Yy", "Qq"], nil],
+      "6":   [["Rr", "Ww"], nil],
+      "7":   [["Bb", "Kk"], nil],
+      "8":   [["Ss", "Bb"], nil],
+      "9":   [["Nn", "Pp"], nil],
+      "10":  [nil, ["Ii", "Hh"]],
+      jack:  [nil, ["Oo", "Ff"]],
+      queen: [nil, ["Aa", "Mm"]],
+      king:  [nil, ["Tt", "Dd"]],
+      ace:   [nil, ["Mm", "Ee"]],
+    }
+    
+    def apply(text, event_values)
+      text = text.dup
+      
+      event_values.tally.each do |card, occurences|
+        index = occurences > 1 ? 1 : 0
+        
+        if substitution = SUBSTITUTIONS.dig(card, index)
+          text.tr! *substitution
+        end
+      end
+      
+      text
+    end
   end
 end
