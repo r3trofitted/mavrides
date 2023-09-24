2ba3f882dbba8b5a33b835867c7a14d022ce9149
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-24 12:59:37 +0300

    Squish instead of stomp
    Heredoc is by definition sensitive about line feeds, while they're only
    used here for ease of read – the tests don't really care about them.

--- test/mailers/messages_mailer_test.rb
+++ test/mailers/messages_mailer_test.rb
@@ -10,19 +10,19 @@ class MessagesMailerTest < ActionMailer::TestCase
     assert_equal ["#{games(:abelar_and_philip).id}@mavrides.example"], mail.from
     assert_equal ["vincent.o@mavrides.example"], mail.to
     
-    body = mail.body.to_s.chomp # I'm not sure why chomp is needed, TBH, but here we are…
+    body = mail.body.to_s.squish
     
     # the Earther's Terrestrial Event is the 1st from the spades table (cf. abelar_and_philip_round_one fixtures)
-    assert_match <<~GAME_EVENT_PROMPT.chomp, body
+    assert_match <<~GAME_EVENT_PROMPT.squish, body
       The first successful demonstration of cryo-sleep technology on rodents raises the potential for human hibernation.
     GAME_EVENT_PROMPT
     
     # the Earther's Personal Event is "7" (cf. abelar_and_philip_round_one fixtures)
-    assert_match <<~PERSONAL_EVENT_PROMPT.chomp, body
+    assert_match <<~PERSONAL_EVENT_PROMPT.squish, body
       Someone got married! Who was it and what do they mean to you?
     PERSONAL_EVENT_PROMPT
     
-    assert_match <<~MESSAGE.chomp, body
+    assert_match <<~MESSAGE.squish, body
       This is it. We are off. To Tau Ceti, can you believe it? Even though I'll never see what's out there, I know.
     MESSAGE
   end
@@ -33,7 +33,7 @@ class MessagesMailerTest < ActionMailer::TestCase
                 .with(message: message)
                 .transmission
     
-    assert_match <<~ENDING_NOTICE.squish, mail.body.to_s.chomp
+    assert_match <<~ENDING_NOTICE.squish, mail.body.to_s.squish
       This event marks the end of the game. Send a final message to #{message.recipient_name}; this message will
       not be distorted. Finish the message with [Connection Lost].
     ENDING_NOTICE
@@ -50,7 +50,7 @@ class MessagesMailerTest < ActionMailer::TestCase
     
     mail = MessagesMailer.with(message: message).transmission
     
-    assert_match <<~DISTORTED_MESSAGE.chomp, mail.body.to_s.chomp
+    assert_match <<~DISTORTED_MESSAGE.squish, mail.body.to_s.squish
       Kig news, you will never kelieve it: my krother got married!
     DISTORTED_MESSAGE
   end
