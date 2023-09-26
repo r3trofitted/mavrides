4fba96062cb0a8623c52a73cdb4bc26e0a5926bd
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-09-26 07:25:42 +0200

    Debug traces

--- app/mailboxes/messages_mailbox.rb
+++ app/mailboxes/messages_mailbox.rb
@@ -7,6 +7,8 @@ class MessagesMailbox < ApplicationMailbox
   before_processing :bounced!, if: -> { game.blank? || sender.blank? }
   
   def process
+    logger.debug "Processing inbound mail"
+    
     content = mail.body.to_s
     
     message = Message.new(sender:, content:, subject: mail.subject)
@@ -14,6 +16,8 @@ class MessagesMailbox < ApplicationMailbox
     begin
       game.messages << message
     rescue UncaughtThrowError
+      logger.debug "failed to register Message (#{message.subject}; #{message.errors.full_messages.join ';'}) to game ##{game.id}."
+      
       bounce_now_with MessagesMailer.with(message:).bounced
     end
   end
@@ -21,6 +25,8 @@ class MessagesMailbox < ApplicationMailbox
   private
   
   def set_game_and_sender
+    logger.debug "Setting game and sender for id##{mail.to} and from: #{mail.from}"
+    
     @game   = Game.started.includes(:earther, :explorer).find_by(id: mail.to.grep(MATCHER) { $1 })
     # OPTIMIZE: we should be able to do better to get the sender with a finder method (Player.character_in_game maybe?)
     @sender = game.characters.find { |c| mail.from.include? c.player_email } unless game.blank?
