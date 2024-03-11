505bc6223b8ef3eb2c3fa34e1de5f0b72a058d6c
Ronan Limon Duparcmeur <ronan@2-45.pm>
2024-03-11 21:54:05 +0100

    Gem update
    + app/config updated with 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,10 @@ module Mavrides
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, see https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#config-autoload-lib-ignore.
-    config.autoload_lib(ignore: %w(assets tasks))
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
