dcb4b086ee0f7fc785cdae24b6c179b21ee787f1
Ronan Limon Duparcmeur <ronan@2-45.pm>
2024-10-26 22:18:41 +0200

    Message logic
    Very little so far. Basically, we only need to determine the _recipient_
    of a message base on its _sender_.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :game
   belongs_to :sender, class_name: "Player"
+
+  delegate :email, to: :sender, prefix: true
+  delegate :email, to: :recipient, prefix: true
+
+  def recipient
+    sender == game.explorer_player ? game.earther_player : game.explorer_player
+  end
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :all
+
+  test "#recipient" do
+    # the first message is sent by Abelar, so the recipient has to be Philip
+    assert_equal messages(:first_message).recipient, players(:vincent)
+  end
 end
