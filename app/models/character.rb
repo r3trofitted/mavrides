7d79f8a13aac5af1d0684d2c8127e010e8af19b8
Ronan Limon Duparcmeur <ronan@2-45.pm>
2024-10-26 18:06:40 +0200

    Added `Game`
    (Which necessitated to add a reference column to the `characters`
    table.)

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :player
-  
+  belongs_to :game
+
   enum :role, %i(earther explorer), validate: true
 end
---
+++ app/models/game.rb
@@ -0,0 +1,15 @@
+class Game < ApplicationRecord
+  with_options class_name: "Character" do
+    has_one :earther, -> { earther }
+    has_one :explorer, -> { explorer }
+  end
+
+  with_options source: :player do
+    has_one :earther_player, through: :earther
+    has_one :explorer_player, through: :explorer
+  end
+
+  enum :status, %i(pending running ended), default: :pending
+
+  validates_presence_of :earther, :explorer
+end
