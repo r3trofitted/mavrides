ea8a4c5a3f915a666c50b716a6572c28209bc0ae
Ronan Limon Duparcmeur <ronan@2-45.pm>
2024-10-26 18:50:11 +0200

    Bouncing emails sent for non-running games
    That's our first 2 tests passing.
    
    Note that the pattern matching with a regexp using `$1` may be a bit too
    arcane for corporate use, buy hey, it's a pet project, I'm allowed to
    have fun!

--- app/mailboxes/messages_mailbox.rb
+++ app/mailboxes/messages_mailbox.rb
@@ -1,4 +1,10 @@
 class MessagesMailbox < ApplicationMailbox
+  before_processing :bounced!, if: -> { game.blank? }
+  
   def process
+  end
+
+  def game
+    @game ||= Game.running.find_by(id: mail.to.grep(/^([\-\w]+)@mavrides.example/) { $1 }) # FIXME: don't hardcode the domain
   end
 end
