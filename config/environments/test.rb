7236052a51c46f9f37b6f81479f066523115c8d8
Ronan Limon Duparcmeur <ronan@2-45.pm>
2024-10-26 17:54:43 +0200

    Configured AR encryption
    It will be needed to store the players' email addresses

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # See https://edgeguides.rubyonrails.org/active_record_encryption.html#fixtures
+  config.active_record.encryption.encrypt_fixtures = true
 end
